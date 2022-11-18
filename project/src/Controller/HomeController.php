d65adac787f2a7cd63e100847750a61cbb7ecdf1
Francois <francois.coche@outlook.it>
2022-11-18 10:52:45 +0100

    ✨ Adding crud for ingredient

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -12,11 +12,6 @@ class HomeController extends AbstractController
     #[Route('/', name:'app_index', methods:['GET'])]
     public function index():Response
     {
-
-        return $this->render('home.html.twig');
-
+        return $this->render('pages/home.html.twig');
     }
-
-
-
 }
